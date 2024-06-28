9bb168113c4a3f5ae6bc4aee4dd09c5d8aa06b9c
tzn <i.makarov@innopolis.university>
2024-06-28 21:20:43 +0300

    added langs change

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:arm_fight_helper/screeens/main_screen.dart';
 import 'package:arm_fight_helper/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'language_notifier.dart';
+import 'l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
@@ -13,11 +15,22 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = ref.watch(themeProvider);
+    final locale = ref.watch(languageProvider);
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ArmFight Helper',
       theme: theme,
+      locale: locale,
+      supportedLocales: [
+        Locale('en'),
+        Locale('ru'),
+      ],
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       home: MainScreen(),
     );
   }
