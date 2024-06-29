420a4e38f4f9b575055932bb77fcbaf534ff8bd1
Resent <remal11151@gmail.com>
2024-06-29 23:48:55 +0300

    Storage implemented

--- lib/screeens/history_screen.dart
+++ lib/screeens/history_screen.dart
@@ -1,25 +1,57 @@
-import 'package:arm_fight_helper/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'main_screen.dart';
+import 'package:arm_fight_helper/widgets/fight_session.dart';
 
-class HistoryScreen extends StatelessWidget {
+class HistoryScreen extends ConsumerWidget {
+  const HistoryScreen({super.key});
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('History'),
-        leading: IconButton(
-          color: const Color.fromARGB(255, 255, 255, 255),
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text(
+                      'Fight History',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ),
+                ]
+              ),
+              SizedBox(height: 16.0),
+              FightCard(
+                fighter1: 'John Doe',
+                fighter2: 'Jane Doe',
+                date: 'Jun 13 2024',
+              ),
+              SizedBox(height: 16.0),
+              FightCard(
+                fighter1: 'John Doe',
+                fighter2: 'Jane Doe',
+                date: 'Jun 13 2024',
+              ),
+              SizedBox(height: 16.0),
+              FightCard(
+                fighter1: 'John Doe',
+                fighter2: 'Jane Doe',
+                date: 'Jun 13 2024',
+              ),
+            ],
+          ),
         ),
-      ),
-      body: Center(
-        child: Text('History of the fights'),
       ),
     );
   }
-  }
+}
+
