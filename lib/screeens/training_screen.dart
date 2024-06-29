971e1661d02266c829f7c12ebc8dfbcb02d585d7
I-y6o-I <mailkirill17@gmail.com>
2024-06-29 20:47:41 +0300

    ~ Resolved naming conflicts v2

--- lib/screeens/training_screen.dart
+++ lib/screeens/training_screen.dart
@@ -1,11 +1,11 @@
-import 'package:arm_fight_helper/widgets/pause_button.dart';
-import 'package:arm_fight_helper/widgets/rounds.dart';
+import 'package:arm_fight_helper/widgets/restart_button_widget.dart';
+import 'package:arm_fight_helper/widgets/rounds_indicator_widget.dart';
 import 'package:arm_fight_helper/widgets/timeCount.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:arm_fight_helper/widgets/start_indicator.dart';
+import 'package:arm_fight_helper/widgets/start_indicator_widget.dart';
 
 import '../l10n/app_localizations.dart';
 
